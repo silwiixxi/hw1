52d2c06d5b9adf85f4c3e9bb3563eb55b929b3ad
silwiixxi <aneliidryshevaa@gmail.com>
2026-01-26 13:29:01 +0500

    Add files via upload

---
+++ hw1.py
@@ -0,0 +1,162 @@
+print("Hello, World!")
+
+import sys
+print(sys.version)
+
+if 5 > 2:
+  print("Five is greater than two!")
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+        print("Five is greater than two!") 
+
+x = 5
+y = "Hello, World!"
+print(x)
+print(y)
+
+#This is a comment.
+print("Hello, World!")
+
+print("Python is fun!")
+
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+print("Hello"); print("How are you?"); print("Bye bye!")
+
+print("Hello World!")
+
+print("Hello World!")
+print("I am learning Python.")
+print("It is awesome!")
+
+print("This will work!")
+print('This will also work!')
+
+print(This will cause an error)
+
+print("Hello World!", end=" ")
+print("I will print on the same line.")
+
+print(3)
+print(358)
+print(50000)
+
+print(3 + 3)
+print(2 * 5)
+
+print("I am", 35, "years old.")
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+x = "John"
+# is the same as
+x = 'John'
+
+a = 4
+A = "Sally"
+#A will not overwrite a
+
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+print(myvar)
+print(my_var)
+print(_my_var)
+print(myVar)
+print(MYVAR)
+print(myvar2)
+
+2myvar = "John"
+my-var = "John"
+my var = "John"
+#This example will produce an error in the result
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+x = "Python is awesome"
+print(x)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+x = 5
+y = 10
+print(x + y)
+
+x = 5
+y = "John"
+print(x + y)
+
+x = 5
+y = "John"
+print(x, y)
+
+x = "awesome"
+def myfunc():
+  print("Python is " + x)
+myfunc()
+
+x = "awesome"
+def myfunc():
+  x = "fantastic"
+  print("Python is " + x)
+
+myfunc()
+print("Python is " + x)
+def myfunc():
+  global x
+  x = "fantastic"
+myfunc()
+print("Python is " + x)
+
+x = "awesome"
+def myfunc():
+  global x
+  x = "fantastic"
+myfunc()
+print("Python is " + x)
+
